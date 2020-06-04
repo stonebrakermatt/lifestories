71a2cba820ae1f1a7190a2f9c4cf1c9393a7eb6f
Matt <stonebrakermatt@gmail.com>
2020-06-04 17:26:12 -0400

    Still bad at design.jpg

--- src/js/components/pages/LandingPage.js
+++ src/js/components/pages/LandingPage.js
@@ -4,12 +4,10 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <div id="home">
-                <img id="bg-img" src="images/lifestoriescompanyphoto.jpeg"/>
                 <div id="home-txt">
                     <h1>Life Stories</h1>
                     <h3>Capturing your story - your legacy</h3> 
                 </div>
-                <button id="viewportfoliobtn">View Portfolio</button>
             </div>
         )
     }
