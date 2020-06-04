d9050d42eb1a207183b9fa788fbd36c371088146
Matt <stonebrakermatt@gmail.com>
2020-06-04 19:00:58 -0400

    Some designs

--- src/js/TitleBar.js
+++ src/js/TitleBar.js
@@ -6,9 +6,10 @@ export default class TitleBar extends React.Component {
             <div id="titlebar">
                 <div id="logo-container">
                     <img id="logo-img" src="/images/ls1500.png"/>
-                    <p id="logo-contact">
-                        214-288-2262
-                    </p>
+                    <div id="logo-contact">
+                        <p id="logo-phone">214-288-2262</p>
+                        <p id="logo-email">chad@lifestories.com</p>
+                    </div>
                     <p id="logo-smallscreen-err">
                         Device too small to load webpage
                     </p>
--- src/js/pages/LandingPage.js
+++ src/js/pages/LandingPage.js
@@ -4,9 +4,14 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <section id="home">
-                <div id="home-txt">
-                    <h1>Life Stories</h1>
-                    <h3>Capturing your story - your legacy</h3> 
+                <div id="home-gradient">
+                    <div id="home-txt">
+                        <h1>Life Stories</h1>
+                        <h3>Capturing your story - your legacy</h3> 
+                        <a id="viewportfoliobtn" href="/">
+                            View Portfolio
+                        </a>
+                    </div>
                 </div>
             </section>
         )
