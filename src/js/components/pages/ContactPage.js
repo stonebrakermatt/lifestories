71cb3e258c1479a04de6a0054daf70c51c6ff383
Matt <stonebrakermatt@gmail.com>
2020-06-04 17:16:41 -0400

    Revert to before I was bad at design

--- src/js/components/pages/ContactPage.js
+++ src/js/components/pages/ContactPage.js
@@ -7,13 +7,13 @@ export default class ContactPage extends React.Component {
                 <p>Contact Section</p>
                 <form id="contact-form">
                     <label id="fname-label" htmlFor="fname">First name:</label>
-                    <input name="fname" id="fname" type="text" placeholder="First Name"/>
+                    <input name="fname" id="fname" type="text" value="First Name"/>
                     <label id="lname-label" htmlFor="lname">Last name:</label>
-                    <input name="lname" id="lname" type="text" placeholder="Last Name"/>
+                    <input name="lname" id="lname" type="text" value="Last Name"/>
                     <label id="subject-label" htmlFor="subject">Subject:</label>
-                    <input name="subject" id="subject" type="text" placeholder="Subject"/>
+                    <input name="subject" id="subject" type="text" value="First Name"/>
                     <label id="request-label" htmlFor="request">Request:</label>
-                    <textarea name="request" id="request" rows="5">
+                    <textarea name="request" id="request" rows="10">
                         Provide additional details here.
                     </textarea>
                     <input id="submit" type="submit" value="Submit"/>
