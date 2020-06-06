92f6d25b3799d0fa517adbc4bfa19be10c0a198b
Matt <stonebrakermatt@gmail.com>
2020-06-06 18:56:37 -0400

    Added in all book photos and testimonial

--- src/js/pages/PortfolioPage.js
+++ src/js/pages/PortfolioPage.js
@@ -7,7 +7,9 @@ export default class PortfolioPage extends React.Component {
 
                 <h1>Portfolio</h1>
                 
-                <div id="project-container">
+                <h2>Memoirs</h2> 
+                
+                <div class="project-container">
                     <div class="project">
                         <img class="projectimg" src="images/portfolio/joeford.jpeg"/>
                         <div class="testimonial-container">
@@ -40,6 +42,237 @@ export default class PortfolioPage extends React.Component {
                             </p>
                             <p class="testimonial-client">-Kelli Herd</p>
                         </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/jacquelineanderson.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Mom was thrilled with her book.  Now our family has
+                                our mother’s legacy documented in a beautiful book. 
+                                We are so proud of it and you should be too.
+                            </p> 
+                            <p class="testimonial-client">-Kevin Anderson</p>
+                        </div>
+                    </div>        
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/stine.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Your work was masterful.  Bob and I are so pleased
+                                with our book!
+                            </p> 
+                            <p class="testimonial-client">-Genie Stine</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/dickburridge.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                I am so happy with the way the book turned out – 
+                                a testament to your dedication and your team’s skills.
+                            </p>
+                            <p class="testimonial-client">-Dick Burridge</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/marvinherb.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Thank you for all your thoughtful efforts in helping
+                                create my husband’s story.  I appreciate the way the
+                                book turned out and we both are proud to present it to
+                                our family and friends as a memoir of his life.  Your
+                                team is extemporary, and each possess the skills and
+                                talent needed to fulfill our dreams of this gift.
+                            </p>
+                            <p class="testimonial-client">-Judy Herb</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/rogerramsey.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                I’m more than thrilled with the end product; the books
+                                are beautiful.  The writing captures the tone and
+                                vision that I wanted for my book.
+                            </p> 
+                            <p class="testimonial-client">-Roger Ramsey</p>
+                        </div>
+                    </div>        
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/markspeese.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Thank you for all your efforts in completing my
+                                memoir.  Your interviewing skills and writing are
+                                very impressive.
+                            </p> 
+                            <p class="testimonial-client">-Mark Speese</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/marvinrush.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Your patience, skills, and professionalism from start
+                                to finish was exceptional.
+                            </p>
+                            <p class="testimonial-client">-Marvin Rush</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/cliftonmorris.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                I’m beyond thrilled.  Your team does work of the
+                                highest quality.
+                            </p>
+                            <p class="testimonial-client">-Clifton Morris</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/bobwaltrip.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/walterjohnson.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/steffenpalko.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/artryan.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/marywright.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/bobwright.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/conradstai.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/donaldedmon.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/johnadams.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/billbenedict.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/daverobson.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/jamesbibler.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/deekelly.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/janeweatherford.jpeg"/>
+                    </div>
+                </div>
+
+                <h2>Company History Books</h2> 
+
+                <div class="project-container">
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/johnsonville.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Just received the book, The Company History of
+                                Johnsonville. We absolutely love it.  It’s truly amazing.
+                            </p> 
+                            <p class="testimonial-client">-Shelly Stayer</p>
+                        </div>
+                    </div> 
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/glazers.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Your team’s writing skills, along with knowledge 
+                                of business and interpersonal skills, produced a book 
+                                that exceeded our expectations.
+                            </p> 
+                            <p class="testimonial-client">-Bennett Glazer</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/anbtexas.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                I want to thank you and your team for the fine
+                                work in completing our company history book.
+                            </p> 
+                            <p class="testimonial-client">-Robert Hulsey</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/nfi.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                I want to tell you that all were impressed with
+                                your interviewing and writing skills, your
+                                professionalism, and patience.  You and your team
+                                did a remarkable job.
+                            </p> 
+                            <p class="testimonial-client">-Ike Brown</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/mcnarosa.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                You did a fantastic job completing our book.
+                                With all three books you produced for us, we have been
+                                very pleased with the quality of your workmanship
+                                and final book assembly.
+                            </p> 
+                            <p class="testimonial-client">-Don McNamara</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/anthonytimberlands.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Your workmanship was encyclopedic.
+                            </p> 
+                            <p class="testimonial-client">-John Ed Anthony</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/judgegroup.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Looks awesome.  You do a fantastic job.
+                                You really have a hack for what you do.
+                            </p> 
+                            <p class="testimonial-client">-Brian Anderson</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/herbertwilliams.jpeg"/>
+                        <div class="testimonial-container">
+                            <p class="testimonial">
+                                Our family is so thankful that we found you and
+                                appreciate all your hard work to produce a wonderful
+                                book that we are so proud of.
+                            </p> 
+                            <p class="testimonial-client">-Herbert Williams (?)</p>
+                        </div>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/standardmeat.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/stripes.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/bobtaylor.jpeg"/>
+                    </div>
+                    <div class="project">
+                        <img class="projectimg" src="images/portfolio/twopotatoes.jpeg"/>
                     </div>
                 </div>
             </section>
