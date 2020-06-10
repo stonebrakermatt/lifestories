a63ef13ee1989cf8e225274fd4a75be36f7763e5
Matt <stonebrakermatt@gmail.com>
2020-06-10 01:25:45 -0400

    show more / show less buttons

--- src/js/pages/landingpage/portfoliosection/PortfolioSubsection.js
+++ src/js/pages/landingpage/portfoliosection/PortfolioSubsection.js
@@ -6,13 +6,20 @@ export default class PortfolioSubsection extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            condensed: true,
             data: PortfolioData.data,
         }
+        this.toggleCondensed.bind(this)
+    }
+    toggleCondensed() {
+        this.setState({
+            condensed: !this.state.condensed
+        })
     }
     render() {
         const portfolioItems = this.state.data
-            .filter(item => item.type == this.props.display)
-            .map(item =>
+        .filter(item => item.type == this.props.display)
+        .map(item =>
             item.hasOwnProperty('testimonial') && item.hasOwnProperty('testimonialBy') ?
                 <PortfolioItem
                     type={item.type}
@@ -28,7 +35,19 @@ export default class PortfolioSubsection extends React.Component {
                     key={item.imgpath}
                 />
         )
-        
+        const filteredPortfolioItems = this.state.data
+        .filter(item => item.type == this.props.display)
+        .filter(item => item.hasOwnProperty('testimonial') && item.hasOwnProperty('testimonialBy'))
+        .map(item =>
+            <PortfolioItem
+                type={item.type}
+                imgpath={item.imgpath}
+                key={item.imgpath}
+                testimonial={item.testimonial}
+                testimonialBy={item.testimonialBy}
+            />
+        )
+
         const subtitle = this.props.display == 0 ?
             "Personal Memoirs"
         :
@@ -39,11 +58,19 @@ export default class PortfolioSubsection extends React.Component {
                     "Family Histories"
                 :
                     "Error: Unknown Category"
+        const subsectionClass = this.state.condensed ? "project-container condensed" : "project-container"
+    
+        const linkText = this.state.condensed ? "Show More" : "Show Less"
+        const displayedPortfolioItems = this.state.condensed ? filteredPortfolioItems : portfolioItems
+
         return (
             <React.Fragment>
                 <h2>{subtitle}</h2>
-                <div className="project-container">
-                    {portfolioItems}
+                <div className={subsectionClass}>
+                    {displayedPortfolioItems}
+                </div>
+                <div className="showmorelessbtn">
+                    <a onClick={() => this.toggleCondensed()}>{linkText}</a>
                 </div>
             </React.Fragment>
         )
